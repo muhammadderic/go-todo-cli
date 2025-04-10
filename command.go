3a669490acb968b1a7b4bb9789ccf5efa79d228f
muhammadderic <muhammad.donny.e@gmail.com>
2025-04-11 06:42:39 +0700

    Create 'edit' a todo by index func

--- command.go
+++ command.go
@@ -3,11 +3,15 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
+	"strconv"
+	"strings"
 )
 
 type CmdFlags struct {
 	Add    string
 	Del    int
+	Edit   string
 	List   bool
 	Toggle int
 }
@@ -24,6 +28,8 @@ func NewCmdFlags() *CmdFlags {
 	)
 	// how to use: go run ./ --toggle=1
 	flag.IntVar(&cf.Toggle, "toggle", -1, "Specify todo by index to toggle complete true/false")
+	// how to use: go run ./ --edit=1:"New title"
+	flag.StringVar(&cf.Edit, "edit", "", "Edit a todo by index & specify a new title. id:new_title")
 	// how to use: go run ./ --del=1
 	flag.IntVar(&cf.Del, "del", -1, "Specify todo by index to delete")
 	// how to use: go run ./ --list
@@ -41,6 +47,19 @@ func (cf *CmdFlags) Execute(todos *Todos) {
 		todos.add(cf.Add)
 	case cf.Toggle != -1:
 		todos.toggle(cf.Toggle)
+	case cf.Edit != "":
+		parts := strings.SplitN(cf.Edit, ":", 2)
+		if len(parts) != 2 {
+			fmt.Println("Error: Invalid format for edit. Please use index:new_title")
+			os.Exit(1)
+		}
+		index, err := strconv.Atoi(parts[0])
+		if err != nil {
+			fmt.Println("Error: Invalid index for edit.")
+			os.Exit(1)
+
+		}
+		todos.edit(index, parts[1])
 	case cf.Del != -1:
 		todos.delete(cf.Del)
 	default:
--- todo.go
+++ todo.go
@@ -59,6 +59,18 @@ func (todos *Todos) toggle(index int) error {
 	return nil
 }
 
+func (todos *Todos) edit(index int, title string) error {
+	t := *todos
+
+	if err := t.validateIndex(index); err != nil {
+		return err
+	}
+
+	t[index].Title = title
+
+	return nil
+}
+
 func (todos *Todos) delete(index int) error {
 	t := *todos
 
